e56196519b4f866df95958b92480ebf8afa966c8
Nelson Chen <crazysim@gmail.com>
2011-10-21 23:47:34 -0700

    team name replacement

--- myscript.js
+++ myscript.js
@@ -2,3 +2,40 @@
 $('tr>th:nth-child(2)').hide()
 
 // Hide R06
+$('tr>td>font').css('color','red').replaceWith(function(e){
+    var str = $(this).text()
+    str = str.substr(4)
+    str = str.trim()
+    teamstr = str
+    str = "<b>" + str + "</b>"
+    if (teamstr == "SparkleMotion"){
+        str = appendname(str,"Some Dude, a Girl, and Pizza")
+    }
+    if (teamstr == "Team4"){
+        str = appendname(str,"Some Dude, a Girl, and Pizza")
+    }
+    if (teamstr == "ShortSited"){
+        str = appendname(str,"Some Dude, a Girl, and Pizza")
+    }
+    if (teamstr == "TeamJacob"){
+        str = appendname(str,"Some Dude, a Girl, and Pizza")
+    }
+    if (teamstr == "AntiCasuals"){
+        str = appendname(str,"Some Dude, a Girl, and Pizza")
+    }
+    if (teamstr == "BobbaMilkTEA"){
+        str = appendname(str,"Some Dude, a Girl, and Pizza")
+    }
+    if (teamstr == "ATwoManTeam"){
+        str = appendname(str,"Some Dude, a Girl, and Pizza")
+    }
+    if (teamstr == "SilverbackCodeMonkys"){
+        str = appendname(str,"Some Dude, a Girl, and Pizza")
+    }
+    return str
+})
+
+function appendname(istr, append){
+    istr = istr + '<font size="1" ><br>* ' + append + "</font>"
+    return istr
+}
